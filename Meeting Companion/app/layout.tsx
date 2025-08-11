10a05dbe1e0e3c50e7a8b411411252bbd2212104
GA1972 <gieve.acidwalla@gmail.com>
2025-08-11 07:31:55 +0530

    Delete Meeting Companion directory

--- Meeting Companion/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "./../styles/globals.css";
-export const metadata = { title: "Meeting Companion", description: "Private meeting bot and summaries" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
